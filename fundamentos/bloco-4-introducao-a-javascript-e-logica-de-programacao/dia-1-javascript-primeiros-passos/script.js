3f381c058bba9c88ca61c102494a48c6e0d66b54
Felipe <fe.donatto@outlook.com>
2022-04-06 19:10:55 -0300

    5

--- fundamentos/bloco-4-introducao-a-javascript-e-logica-de-programacao/dia-1-javascript-primeiros-passos/script.js
+++ fundamentos/bloco-4-introducao-a-javascript-e-logica-de-programacao/dia-1-javascript-primeiros-passos/script.js
@@ -1,10 +1,12 @@
-const a = 5
+const a = 60
+const b = 30
+const c = 90
 
 
-if(a>0){
-    console.log("positive")
-} else if (a<0){
-    console.log("negative")
+if(a<0||b<0||c<0){
+    console.log("error")
+} else if(a+b+c>180||a+b+c<180){
+    console.log(false+", seus não formam um triangulo")
 } else {
-    console.log("zero")
+    console.log(true+", seus dados formam um triangulo")
 }
