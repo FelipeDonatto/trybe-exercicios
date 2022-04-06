e0f115726a2f2c4fa5bb5bce58ca893d29868d71
Felipe <fe.donatto@outlook.com>
2022-04-06 18:51:39 -0300

    4

--- fundamentos/bloco-4-introducao-a-javascript-e-logica-de-programacao/dia-1-javascript-primeiros-passos/script.js
+++ fundamentos/bloco-4-introducao-a-javascript-e-logica-de-programacao/dia-1-javascript-primeiros-passos/script.js
@@ -1,12 +1,10 @@
 const a = 5
-const b = 3
-const c = 9
 
-if(a>b &&a>c){
-    console.log(a)
-    
-} else if (b>c &&b>a){
-    console.log(b)
-} else{
-    console.log(c)
+
+if(a>0){
+    console.log("positive")
+} else if (a<0){
+    console.log("negative")
+} else {
+    console.log("zero")
 }
