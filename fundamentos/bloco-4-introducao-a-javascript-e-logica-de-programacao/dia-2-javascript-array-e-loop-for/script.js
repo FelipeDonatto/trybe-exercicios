9a148dc6a1e351b721d78b566778cecc5e8b7fde
Felipe <fe.donatto@outlook.com>
2022-04-07 19:30:05 -0300

    atividade

---
+++ fundamentos/bloco-4-introducao-a-javascript-e-logica-de-programacao/dia-2-javascript-array-e-loop-for/script.js
@@ -0,0 +1,57 @@
+let numbers = [5, 9, 3, 19, 70, 8, 100, 2, 35, 27];
+
+// for(let num of numbers){
+//     console.log(num)
+// }
+// exercicio 1
+
+// let soma = 0
+// for(let i = 0;i<numbers.length;i++){
+//     soma = soma+numbers[i]
+// }
+// console.log(soma)
+// exercicio 2
+
+// let soma = 0
+// let quant = 0
+// for(let i = 0;i<numbers.length;i++){
+//     soma = soma+numbers[i]
+//     quant = quant+i
+// }
+// console.log(soma/quant)
+// exercicio 3
+
+// let soma = 0
+// let quant = 0
+// for(let i = 0;i<numbers.length;i++){
+//     soma = soma+numbers[i]
+//     quant = quant+i
+// }
+// if(soma/quant > 20){
+//     console.log(soma/quant +'O valor é maior que 20')
+// }else{
+//     console.log(soma/quant +'O valor é menor que 20')
+// }
+// exercicio 4
+
+// let higher = 0
+// for(i=0;i<numbers.length;i++){
+//     if (numbers[i] > higher)
+//     higher = numbers[i]
+// }
+// console.log(higher)
+// exercicio 5
+
+// let odd = []
+// for(i=0; i<numbers.length; i++){
+//             if(numbers[i]%2 === 0){    
+//         }else{
+//             odd.push(numbers[i])
+//     }
+// }
+// if(odd == 0){
+//     console.log('nao existem numeros impares')
+// } else {
+//     console.log(odd +' são numeros impares')
+// }
+// exercicio 6
